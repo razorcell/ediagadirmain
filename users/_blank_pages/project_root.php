d90035c9350e934bc65e4a3c87d05dd5e56f317d
Khalifa rmili <khalifa.rmili@gmail.com>
2019-04-19 18:12:46 +0100

    nothing special: just added this file

--- users/_blank_pages/project_root.php
+++ users/_blank_pages/project_root.php
@@ -18,23 +18,22 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 require_once 'users/init.php';
-require_once $abs_us_root.$us_url_root.'users/includes/template/prep.php';
-if (!securePage($_SERVER['PHP_SELF'])){die();}
+require_once $abs_us_root . $us_url_root . 'users/includes/template/prep.php';
+if (!securePage($_SERVER['PHP_SELF'])) {
+	die();
+}
 ?>
 
 <?php
 //php goes here
 ?>
 
-<div id="page-wrapper">
-	<div class="container">
-		<div class="row">
-			<div class="col-sm-12">
+<div class="row">
+	<div class="col-sm-12">
 
-			</div>
-		</div>
 	</div>
 </div>
+
 
 
 <?php require_once $abs_us_root . $us_url_root . 'users/includes/html_footer.php'; ?>
